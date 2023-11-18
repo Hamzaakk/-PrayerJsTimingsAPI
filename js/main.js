b4d78bcf3dcd3ba0e8dde68f0e0ab89239791459
AKKAOUI HAMZA <73229553+Hamzaakk@users.noreply.github.com>
2023-11-19 00:30:59 +0100

    Update main.js
    add code js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,26 @@
+axios.get('http://api.aladhan.com/v1/timingsByCity?country=MA&city=Rabat&method=4')
+  .then(function (response) {
+    // handle success
+    console.log(response.data.data);
+    let datenow = response.data.data.date.readable
+    document.getElementById('nowdate').innerText = datenow
 
+    console.log(response.data.data.timings.Fajr)
+    const timings = response.data.data.timings
+    fillTime("fajr",timings.Fajr)
+    fillTime("dohr",timings.Dhuhr)
+    fillTime("asr",timings.Asr)
+    fillTime("mghrib",timings.Maghrib)
+    fillTime("asha",timings.Isha)
+  })
+  .catch(function (error) {
+    // handle error
+    console.log(error);
+  })
+  .finally(function () {
+    // always executed
+  });
+  
+  function fillTime(id,time){
+    document.getElementById(id).innerText =time
+  }
